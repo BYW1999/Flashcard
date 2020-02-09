a4c39cbaed06d6ba9eeadb75b18d42a60c0eaeba
BYW1999 <1434119041@qq.com>
2020-02-09 17:50:34 +0800

    2020/2/9 数字练习-获取全部题目

--- JavaProject/Flashcard/src/main/java/com/byw/flashcard/controller/NumberController.java
+++ JavaProject/Flashcard/src/main/java/com/byw/flashcard/controller/NumberController.java
@@ -33,9 +33,9 @@ public class NumberController {
     /**
      * 获取练习题
      * @param grade（1：简单、2：中等、3：困难）
-     * @return 题目、正确答案、选项
+     * @return 题目、正确答案、选项（每次获取一道题）
      */
-    @RequestMapping("/getNumQuestion")
+    /*@RequestMapping("/getNumQuestion")
     public JSONObject getNumQuestion(@RequestParam(value="grade",defaultValue="") String grade){
         List<String> numList = numberMapper.findAllNum();
         ArrayList<String> optionList = new ArrayList<>();
@@ -103,8 +103,121 @@ public class NumberController {
             }
         }
         return jsonObject;
-    }
+    }*/
 
+    /**
+     * 获取练习题
+     * @param grade（1：简单、2：中等、3：困难）
+     * @return 题目、正确答案、选项(一次全部获取)
+     */
+    @RequestMapping("/getNumQuestion")
+    public JSONObject getNumQuestion(@RequestParam(value="grade",defaultValue="") String grade){
+        List<String> numList = numberMapper.findAllNum();
+        Map<String,Object> allMap = new HashMap<>();
+        JSONObject jsonObject = new JSONObject();
+        Random random = new Random();
+
+        if (grade.equals("1")){    //5个题目，每道题目4个选项，其中包含一个正确选项和一个错误选项
+            for(int j=0;j<5;j++){
+                ArrayList<String> optionList = new ArrayList<>();   //错误选项列表
+                int n = random.nextInt(numList.size());
+                Map<String,Object> map = new HashMap<>();
+                String question = numList.get(n);
+                map.put("question", question);
+                map.put("answer", question);
+                System.out.println("question========="+question);
+                for(int i=0;i<3;i++){
+                    int m = random.nextInt(numList.size());
+                    String error = numList.get(m);
+                    if (!error.equals(question)){
+                        optionList.add(error);
+                    }else {
+                        int error1 = Integer.valueOf(error)+1;
+                        optionList.add(String.valueOf(error1));
+                    }
+                }
+                map.put("errorList", optionList);
+                allMap.put(String.valueOf(j+1), map);
+            }
+
+        }else if(grade.equals("2")){    //10个数字，选出上一位、下一位
+            String[] lastOrNext = {"0","1"};    //"0":上一位 "1"下一位
+            for(int o=0;o<10;o++){
+                ArrayList<String> optionList = new ArrayList<>();
+                Map<String,Object> map = new HashMap<>();
+                int j = random.nextInt(lastOrNext.length);
+                String randLN = lastOrNext[j];
+                int n = random.nextInt(numList.size());
+                String question = numList.get(n);
+                String answer = "";
+                if(randLN.equals("0")){    //上一位
+                    String questionDes = question + "的上一个数字是什么？";
+                    map.put("question", questionDes);
+                    answer = String.valueOf(Integer.valueOf(question)-1);
+                    map.put("answer", answer);
+                }else if (randLN.equals("1")){  //下一位
+                    String questionDes = question + "的下一个数字是什么？";
+                    map.put("question", questionDes);
+                    answer = String.valueOf(Integer.valueOf(question)+1);
+                    map.put("answer", answer);
+                }
+                for(int i=0;i<3;i++){
+                    int m = random.nextInt(numList.size());
+                    String error = numList.get(m);
+                    if (!error.equals(answer)){
+                        optionList.add(error);
+                    }else {
+                        int error1 = Integer.valueOf(error)+2;
+                        optionList.add(String.valueOf(error1));
+                    }
+                }
+                map.put("errorList", optionList);
+                allMap.put(String.valueOf(o+1), map);
+            }
+
+        }else if(grade.equals("3")){    //10个，第二个或者第三个
+            String[] lastOrNext = {"0","1","2"};    //"0":上一位 "1"下一位，“2”没有
+            for(int o=0;o<10;o++){
+                ArrayList<String> optionList = new ArrayList<>();
+                Map<String,Object> map = new HashMap<>();
+                int j = random.nextInt(lastOrNext.length);
+                String randLN = lastOrNext[j];
+                int n = random.nextInt(numList.size());
+                String question = numList.get(n);
+                String answer = "";
+                if(randLN.equals("0")){    //上一位
+                    String questionDes = question + "的上一个数字是什么？";
+                    map.put("question", questionDes);
+                    answer = String.valueOf(Integer.valueOf(question)-1);
+                    map.put("answer", answer);
+                }else if (randLN.equals("1")){  //下一位
+                    String questionDes = question + "的下一个数字是什么？";
+                    map.put("question", questionDes);
+                    answer = String.valueOf(Integer.valueOf(question)+1);
+                    map.put("answer", answer);
+                }else if(randLN.equals("2")){   //不设置上一位或者下一位
+                    map.put("question", question);
+                    map.put("answer", question);
+                }
+                for(int i=0;i<3;i++){
+                    int m = random.nextInt(numList.size());
+                    String error = numList.get(m);
+                    if (!error.equals(answer)){
+                        optionList.add(error);
+                    }else {
+                        int error1 = Integer.valueOf(error)-2;
+                        optionList.add(String.valueOf(error1));
+                    }
+                }
+                map.put("errorList", optionList);
+                allMap.put(String.valueOf(o+1), map);
+            }
+        }else {
+            allMap.put("error", "参数异常！");
+        }
+        jsonObject = new JSONObject(allMap);
+        return jsonObject;
+    }
 
     /**
      *
